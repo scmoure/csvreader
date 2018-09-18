bcf081b5b629630c24b4b4ca80a8af3f95a3fcaf
Santiago Carretero <scmoure@gmail.com>
2018-09-18 20:09:24 +0200

    StringMapper class visibility

--- src/main/java/com/scmoure/csvreader/mapper/StringMapper.java
+++ src/main/java/com/scmoure/csvreader/mapper/StringMapper.java
@@ -2,7 +2,7 @@ package com.scmoure.csvreader.mapper;
 
 import com.scmoure.csvreader.AtomicMapper;
 
-public class StringMapper implements AtomicMapper {
+class StringMapper implements AtomicMapper {
 
 	@Override
 	public String apply(String rawValue) {
